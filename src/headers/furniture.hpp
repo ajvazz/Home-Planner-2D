260cf2d2b63badfaaabe16168351379f53892b1f
Nevena Ajvaz <nevena.ajvaz@hotmail.com>
2018-12-26 22:27:37 +0100

    Added hover events and selected mode
    - when cursor is hovered over furniture, it changes shape
    - when furniture is selected, a green square is drawn behind it

--- src/headers/furniture.hpp
+++ src/headers/furniture.hpp
@@ -2,6 +2,7 @@
 #define FURNITURE_HPP
 
 #include <QGraphicsItem>
+#include <QPen>
 
 class Furniture : public QGraphicsItem
 {
@@ -13,8 +14,19 @@ protected:
                QWidget *widget = nullptr) override;
 
     QRectF boundingRect() const override;
-
     void keyPressEvent(QKeyEvent *event) override;
+    void mousePressEvent(QGraphicsSceneMouseEvent * event) override;
+    void hoverEnterEvent(QGraphicsSceneHoverEvent *event) override;
+    void hoverLeaveEvent(QGraphicsSceneHoverEvent *event) override;
+
+    void setRotation(qreal angle);
+    void drawOutline();
+
+    /* Variables */
+
+private:
+    QPen m_pen;
+    qreal angle;
 };
 
 #endif // FURNITURE_HPP
--- src/source/furniture.cpp
+++ src/source/furniture.cpp
@@ -1,20 +1,36 @@
 #include <QtGui>
+#include <QApplication>
+#include <QDesktopWidget>
 
+#include <QtDebug>
 #include "../headers/furniture.hpp"
 
 Furniture::Furniture(QGraphicsItem *parent) : QGraphicsItem(parent)
 {
-    // Testing...
-    setFlag(QGraphicsItem::ItemIsMovable);
-    setFlag(QGraphicsItem::ItemIsFocusable);
+    setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsFocusable |
+             QGraphicsItem::ItemIsSelectable);
+    setAcceptHoverEvents(true);
+
+    angle = 0;
+
+    /* Setting position to center of scene (screen) */
+    int screenWidth  = QApplication::desktop()->width();
+    int screenHeight = QApplication::desktop()->height();
+    setPos(screenWidth/2, screenHeight/2);
 }
 
 void Furniture::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    // Testing...
-    Q_UNUSED(option);
-    Q_UNUSED(widget);
-    painter->drawPixmap(0,0,75,75, QPixmap(":/img/furniture/beds/king_bed_1_white.png"));
+    Q_UNUSED(option); Q_UNUSED(widget);
+
+    if (isSelected()) {
+        painter->setPen(m_pen);
+        m_pen.setWidth(2);
+        m_pen.setColor(Qt::green);
+        painter->drawRect(boundingRect());
+    }
+
+    painter->drawPixmap(0,0,75,75, QPixmap(":/img/furniture/tables/table_3_round_light_wood.png"));
 }
 
 QRectF Furniture::boundingRect() const
@@ -25,7 +41,6 @@ QRectF Furniture::boundingRect() const
 
 void Furniture::keyPressEvent(QKeyEvent *event)
 {
-    // Testing...
     switch (event->key())
     {
         case Qt::Key_Right:
@@ -41,5 +56,30 @@ void Furniture::keyPressEvent(QKeyEvent *event)
             moveBy(0, 10);
             break;
     }
+
     update();
+}
+
+void Furniture::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    QGraphicsItem::mousePressEvent(event);
+    this->setFocus();
+}
+
+void Furniture::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
+{
+    Q_UNUSED(event);
+    QApplication::setOverrideCursor(Qt::PointingHandCursor);
+}
+
+void Furniture::hoverLeaveEvent(QGraphicsSceneHoverEvent *event)
+{
+    Q_UNUSED(event);
+    QApplication::restoreOverrideCursor();
+}
+
+// Angle normalization is not needed, Qt already does that
+void Furniture::setRotation(qreal angle)
+{
+    QGraphicsItem::setRotation(angle);
 }
