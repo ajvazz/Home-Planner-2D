f8f9b12d2869736da071f84ba433927083c68832
Nevena Ajvaz <nevena.ajvaz@hotmail.com>
2018-12-31 15:57:18 +0100

    Constructor accepts custom path; rotation
    - This has enabled custom textures of floor
    - Room rotation is added and its shortcut

--- src/headers/room.hpp
+++ src/headers/room.hpp
@@ -2,20 +2,28 @@
 #define ROOM_HPP
 
 #include <QGraphicsItem>
+#include <QPen>
 
 class Room : public QGraphicsItem
 {
 public:
-    Room(int width, int height);
+    Room(int width, int height, QString urlPath);
 
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
                QWidget *widget = nullptr) override;
 
     QRectF boundingRect() const override;
 
+    void setFloorPath(QString urlP);
+    QString floorPath() const;
+    void rotate(qreal angleParam);
+
 private:
+    qreal angle;
     int m_width;
     int m_height;
+    QPen m_pen;
+    QString m_urlPath;
 };
 
 #endif // ROOM_HPP
--- src/source/room.cpp
+++ src/source/room.cpp
@@ -5,8 +5,8 @@
 
 #include "../headers/room.hpp"
 
-Room::Room(int width, int height)
-    : m_width(width), m_height(height)
+Room::Room(int width, int height, QString urlPath)
+    : m_width(width), m_height(height), m_urlPath(urlPath)
 {
     setFlags(ItemIsMovable | ItemIsFocusable | ItemIsSelectable);
 
@@ -17,13 +17,50 @@ Room::Room(int width, int height)
 
 }
 
+void Room::setFloorPath(QString urlP)
+{
+    this->m_urlPath = urlP;
+}
+
+QString Room::floorPath() const
+{
+    return m_urlPath;
+}
+
+void Room::rotate(qreal angleParam)
+{
+    setTransformOriginPoint(m_width/2, m_height/2);
+    angle += angleParam;
+    setRotation(angle);
+}
+
 void Room::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     Q_UNUSED(option); Q_UNUSED(widget);
 
-    QColor *myColor = new QColor(71, 71, 71, 255);
+    if (isSelected()) {
+        painter->setPen(m_pen);
+        m_pen.setWidth(3);
+        m_pen.setColor(Qt::green);
+        painter->drawRect(boundingRect());
+    }
+
+    if (m_urlPath.isEmpty()) {
+        /* Default grey floor */
+        QColor *myColor = new QColor(171, 171, 171, 255);
+        painter->fillRect(boundingRect(), myColor->rgb());
+    }
+    else {
+
+        /* The user has chosen a floor texture, setFloorPath has been set
+         * with an appropriate path, now it is not empty and can be drawn */
+
+        QBrush brush(QPixmap(m_urlPath).scaled(35, 35));
+        painter->setBrush(brush);
+        painter->drawRect(boundingRect());
+    }
+
     painter->drawRect(0, 0, m_width, m_height);
-    painter->fillRect(boundingRect(), myColor->rgb());
 }
 
 QRectF Room::boundingRect() const
