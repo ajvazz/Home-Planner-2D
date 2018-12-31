413ccb84d8439af2cb1a1e10ba45b9e2e988a3bf
Nenad Ajvaz <nenadajvaz5@gmail.com>
2018-12-31 01:16:31 +0100

    Changes in instructions field

--- src/source/main_menu_window.cpp
+++ src/source/main_menu_window.cpp
@@ -65,7 +65,37 @@ void MainMenuWindow::on_btnAbout_clicked()
 
 void MainMenuWindow::on_btnInstructions_clicked()
 {
-     QMessageBox::about(this, "Instructions", "To be done...");
+    QMessageBox::about(this, "Instructions",
+        "MAIN MENU\n\n"
+        "Upon clicking 'Create New', the user is prompted with a choice between "
+        "creating everything from the beginning or choosing a pre-made template.\n\n"
+
+        "START FROM SCRATCH (not available yet)\n"
+        ". . .\n\n"
+
+        "CHOOSE A TEMPLATE\n\n"
+        "(1) The right part of the working area is a sorted 'catalog' of furniture.\n"
+        "(2) The left part of the working area is the current scheme and output, on which "
+        "the user can interact with furniture.\n"
+        "(3) On the bottom-left are the controls for that interaction.\n\n"
+
+        "(1) Here the user can choose from a range of various pieces of furniture. "
+        "For some hardly recognizable furniture, the user can hover over its icon for "
+        "some short description. Items are created by clicking in the catalog and they "
+        "will always appear in the center of the scheme.\n\n"
+
+        "(2) This is the working area. Here the furniture will be created and manipulated "
+        "with. Item and scene manipulation is under (3).\n"
+        "When the item on is selected, a green outlined is shown. Multi selection is "
+        "supported, which can be done in a standard way : CTRL + left click, and "
+        "everything selected is treated as a single item.\n\n"
+
+        "(3) Top row of buttons controls the scene, and the bottom row controls furniture."
+        "Supported transformations are rotation, translation and zooming."
+        "Hovering over control buttons will create a helpful bubble.\n\n"
+
+        "List of shortcuts can be seen in menu bar or by clicking CTRL + S when the program starts.\n"
+    );
 }
 
 void MainMenuWindow::on_btnTemplate_clicked()
